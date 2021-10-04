cc521323d3529ac27c5b21e55acf4487e8379746
austinmp <apbeermoney@gmail.com>
2021-10-03 20:13:37 -0400

    Solution for problem 209. Minimum Size Subarray Sum

---
+++ javascript/minimum-size-subarray-sum.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let min = Number.POSITIVE_INFINITY;
+    let isSolution = false;
+    let left = 0, sum = 0;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] >= target) return 1;
+        sum += nums[i];
+        if(sum >= target) {
+            isSolution = true;
+            while(sum - nums[left] >= target ) {
+                sum -= nums[left];
+                left++;
+            } 
+            min = Math.min(min, i - left + 1);
+        } 
+    }
+    if(!isSolution) return 0;
+    return min;
+};
