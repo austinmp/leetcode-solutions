8c4dcdc138e3fe1b130b4704803fea39c02d7a26
austinmp <48191538+austinmp@users.noreply.github.com>
2021-10-18 13:42:23 -0400

    Cleaned up union find implementation

--- javascript/number-of-connected-components-in-an-undirected-graph.js
+++ javascript/number-of-connected-components-in-an-undirected-graph.js
@@ -3,45 +3,25 @@
  * @param {number[][]} edges
  * @return {number}
  */
-var countComponents = function(n, edges) {
-    let components = 0;
-    const union = new Array(n).fill(-1);
-
-    edges.forEach( (edge) => {
-        let a = edge[0];
-        let b = edge[1];
-        if( union[a] === -1 && union[b] === -1 ) {
-            union[a] = a;
-            union[b] = a;
-        } else if (union[a] >= 0 && union[b] >= 0 && union[a] != union[b]) {
-            const [rootIndexA, componentsInA] = componentSize(a, union);
-            const [rootIndexB, componentsInB] = componentSize(b, union);
-            if(componentsInA >= componentsInB ) {
-                union[rootIndexB] = union[rootIndexA];
-            } else if ( componentsInB  > componentsInA) {
-                union[rootIndexA] = union[rootIndexB];
-            } 
-        } else if ( union[a] >= 0 || union[b] >= 0 ) {
-            union[a] = union[a] >= 0 ? union[a] : union[b];
-            union[b] = union[b] >= 0 ? union[b] : union[a];
-        }
-    });
+ var countComponents = function(n, edges) {
+    let componentsCount = 0;
+    if(n < 2) return n;
     
-    for(let i = 0; i < n; i++){
-        if(union[i] === i) components++;
-        if(union[i] === -1) components++;
-    }
-
-    return components;       
+    const components = new Array(n);
+    
+    for(let i = 0; i < n; i++) components[i] = i;
+    
+    edges.forEach( ([a,b]) => components[root(a, components)] = root(b, components) );
+    
+    for(let i = 0; i < n; i++) if(components[i] === i) componentsCount++;
+    
+    return componentsCount; 
 };
 
-const componentSize = (index, union) => {
-    let size = 1;
-
-    while(index != union[index]) {
-        size++;
-        index = union[index];
+const root = (i, components) => {
+    while(i != components[i]) {
+        components[i] = components[components[i]]
+        i = components[i];
     }
-    
-    return [index, size];
+    return i;
 }
