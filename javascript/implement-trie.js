9e871c6d931c86be75bc2a2e4cc240ca1393a130
austinmp <48191538+austinmp@users.noreply.github.com>
2021-10-25 14:40:37 -0400

    Updated solution for implement trie

--- javascript/implement-trie.js
+++ javascript/implement-trie.js
@@ -1,49 +1,46 @@
-class Trie {   
+class Trie  {
     constructor() {
-        this.root = new this.Node(false);   
+        this.root = new this.TrieNode();
     }
-
-    Node = class {
+    
+    TrieNode = class {
         constructor(isTerminal) {
-            this.pointers = new Array(26);
             this.isTerminal = isTerminal || false;
+            this.edges = new Array(26);
         }
     }
 
     insert = (word) => {
+        if(this.search(word)) return;
+
         let curr = this.root;
         for(let i = 0; i < word.length; i++) {
-            // map to value between 0-25 (a-z)
-            let index = word.charCodeAt(i) - 97;
-            if(!curr.pointers[index]) {
-                curr.pointers[index] = new this.Node();
+            let index = word.charCodeAt(i) - 'a'.charCodeAt(0);
+            if(!curr.edges[index]){
+                curr.edges[index] =  new this.TrieNode();
             }
-            curr = curr.pointers[index];
+            curr = curr.edges[index];
+            // handles case where the word we are adding is prefix of another word (e.g. we have 'apple' and we add 'app')
+            if(i === word.length-1) curr.isTerminal = true; 
         }
-        curr.isTerminal = true;
     }
 
+    searchPrefix = (word) => {
+        let curr = this.root;
+        for(let i = 0; i < word.length; i++) {
+            let index = word.charCodeAt(i) - 'a'.charCodeAt(0);
+            if(!curr.edges[index]) return null;
+            curr = curr.edges[index];
+        }
+        return curr;
+    }
+    
     search = (word) => {
-        let curr = this.root;
-        for(let i = 0; i < word.length; i++) {
-            let index = word.charCodeAt(i) - 97;
-            if(!curr.pointers[index]) {
-                return false;
-            }
-             curr = curr.pointers[index];
-        }
-        return curr.isTerminal;
+        const lastChar = this.searchPrefix(word);
+        return (lastChar !== null && lastChar.isTerminal);
     }
 
-    startsWith = (word) => {
-        let curr = this.root;
-        for(let i = 0; i < word.length; i++) {
-            let index = word.charCodeAt(i) - 97;
-            if(!curr.pointers[index]) {
-                return false;
-            }
-             curr = curr.pointers[index];
-        }
-        return true;
+    startsWith = (prefix) => {
+        return (this.searchPrefix(prefix) !== null)
     }
 }
